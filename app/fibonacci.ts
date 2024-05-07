3f669539268e1b43ca27d924c718ac306dd80d93
masa-massara <masaya.yoshikawa.31@icloud.com>
2024-05-08 06:07:38 +0900

    feat: フィボナッチ数列の計算をするfibonacci.tsを作成

---
+++ app/fibonacci.ts
@@ -0,0 +1,22 @@
+/**
+ * フィボナッチ数列の演算を行う関数です。
+ * 丸め込みによる誤差を減らすためにBigInt型を用いています。
+ */
+
+export const calculate_fibonacci = (position: number) => {
+  // クエリが1の場合は1を返す
+  if (position === 1) {
+    return { result: "1" };
+  }
+
+  let current = BigInt(0);
+  let next = BigInt(1);
+  let sum = BigInt(0);
+  for (let index = 1; index < position; index++) {
+    sum = current + next;
+    current = next;
+    next = sum;
+  }
+
+  return { result: sum.toString() };
+};
