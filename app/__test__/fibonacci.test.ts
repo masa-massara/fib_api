10d8c1b95d8b7bb769df8cdfdcc08ed4e086f882
masa-massara <masaya.yoshikawa.31@icloud.com>
2024-05-08 06:12:45 +0900

    test: フィボナッチ数列の計算をテストするfibonacci.test.tsを作成

---
+++ app/__test__/fibonacci.test.ts
@@ -0,0 +1,21 @@
+import { calculate_fibonacci } from "../fibonacci";
+
+describe("フィボナッチ数列の計算", () => {
+  test("クエリが1の時", () => {
+    expect(calculate_fibonacci(1)).toEqual({ result: "1" });
+  });
+
+  test("クエリが2の時", () => {
+    expect(calculate_fibonacci(2)).toEqual({ result: "1" });
+  });
+
+  test("クエリが10の時", () => {
+    expect(calculate_fibonacci(10)).toEqual({ result: "55" });
+  });
+
+  test("クエリが99の時", () => {
+    expect(calculate_fibonacci(99)).toEqual({
+      result: "218922995834555169026",
+    });
+  });
+});
